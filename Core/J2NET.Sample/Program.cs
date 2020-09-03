b7cf55951384109c1aa8b1ee74f670c8476a0e60
Kevin <iodesme@gmail.com>
2020-09-03 19:57:43 +0900

    Implement J2NET sample project

---
+++ Core/J2NET.Sample/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace J2NET.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var process = JavaRuntime.Execute("../../../Sample.jar");
+            process.WaitForExit();
+        }
+    }
+}
---
+++ Core/J2NET/JavaRuntime.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace J2NET
+{
+    public static class JavaRuntime
+    {
+        public static Process Execute(string value)
+        {
+            string java = null;
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                switch (RuntimeInformation.OSArchitecture)
+                {
+                    case Architecture.X64:
+                        java = "win-64";
+                        break;
+
+                    case Architecture.X86:
+                        java = "win-32";
+                        break;
+                }
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                java = "linux";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                java = "mac";
+            }
+
+            return Process.Start(Path.Combine("runtime", java, "bin", "java"), $"-jar {value}");
+        }
+    }
+}
