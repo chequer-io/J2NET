197e9fbdf7bad066cb55ff8655c9d5ee294c64d3
Kevin <iodesme@gmail.com>
2020-09-03 20:07:24 +0900

    Update java sample file

---
+++ Core/J2NET.Sample/Sample/src/com/chequer/sample/Main.java
@@ -0,0 +1,9 @@
+package com.chequer.sample;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String home = System.getProperty("java.home");
+        System.out.println("Hello!\nCurrent jre path is " + home);
+    }
+}
